51b2a20ea0191f7a3801168e6e957d5fc3c14a39
Julie Gumerman <julie.gumerman@gmail.com>
2019-10-07 14:19:41 -0600

    server set up, get request to accounts

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+const express = require("express");
 const server = require('./server.js');
 
-const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+
+// const PORT = process.env.PORT || 6666;
+const port = 1212;
+
+
+server.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
+
+
--- server.js
+++ server.js
@@ -2,8 +2,22 @@ const express = require('express');
 
 const db = require('./data/dbConfig.js');
 
+
 const server = express();
 
 server.use(express.json());
+
+
+server.get("/", (req, res) => {
+    res.send("Haiiii!")
+  })
+
+server.get("/api/accounts", (req, res) => {
+    db
+        .select("*").from("accounts")
+        .then(accounts => res.status(200).json(accounts))
+        .catch(err => res.status(500).json({ error: "You done messed up"}))
+        
+})
 
 module.exports = server;
